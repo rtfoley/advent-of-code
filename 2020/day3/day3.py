d211746733a058ff111ad63b3205d6e83e8c8b80
Ryan Foley <rtfoley@gmail.com>
2020-12-03 11:00:55 -0500

    add day 3 part 2 solution

--- 2020/day3/day3.py
+++ 2020/day3/day3.py
@@ -1,19 +1,34 @@
-def part1(raw_data):
-    trees = 0
-    length = len(raw_data[0])
+import sys
 
-    for i in range(1, len(raw_data), 1):
+def find_trees(raw_data, over, down):
+    trees = 0
+
+    for i in range(down, len(raw_data), down):
         if i >= len(raw_data):
             return trees
 
-        column = i * 3 % (length -1)
+        column = int((i/down) * over % (len(raw_data[0]) -1))
+        #print(f'row {i} column {column} {raw_data[i][column]}')
         if raw_data[i][column] == "#":
             trees += 1
 
     return trees
 
+def part1(raw_data):
+    return find_trees(raw_data, 3, 1)
+
+def part2(raw_data):
+    total_trees = 1
+    slopes = [(1, 1),(3,1),(5,1),(7,1),(1,2)]
+    for over, down in slopes:
+        trees = find_trees(raw_data, over, down)
+        print(f'{trees} for {over} over {down} down')
+        total_trees *= trees
+
+    return total_trees
 
 if __name__ == "__main__":
-    with open("input.txt", "r") as raw:
+    with open(sys.argv[1], "r") as raw:
         data = raw.readlines()
         print("#1: ", part1(data))
+        print("#2: ", part2(data))
