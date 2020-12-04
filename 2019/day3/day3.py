180f2ff04358ca5c51b6635bb3ffdfe01f1d2cf3
Ryan Foley <rtfoley@gmail.com>
2020-12-04 14:05:28 -0500

    add 2019 day 3 part 2 solution

--- 2019/day3/day3.py
+++ 2019/day3/day3.py
@@ -1,27 +1,34 @@
 import sys
 import math
 
-def get_points_for_segment(start, segment):
-    points = [(start)]
-    for i in range(1, int(segment[1:]) + 1):        
+def get_points_for_segment(start, segment, steps):
+    points = {(start): steps}
+    for i in range(1, int(segment[1:]) + 1): 
+        steps += 1
+        point = (start[0], start[1])
+        last_point =list(points)[-1]
         if segment[0] == 'R':
-            points.append((points[-1][0] + 1, points[-1][1]))
+            point = (last_point[0] + 1, last_point[1])
         elif segment[0] == 'L':
-            points.append((points[-1][0] - 1, points[-1][1]))
+            point = (last_point[0] - 1, last_point[1])
         if segment[0] == 'U':
-            points.append((points[-1][0], points[-1][1] + 1))
+            point = (last_point[0], last_point[1] + 1)
         elif segment[0] == 'D':
-            points.append((points[-1][0], points[-1][1] - 1))
+            point = (last_point[0], last_point[1] - 1)
+
+        points[point] = steps
 
     return points
 
 def get_points(wire):
-    points = []
+    points = {}
+    steps = 0
     startingPoint = (0, 0)
     for segment in wire:
-        new_points = get_points_for_segment(startingPoint, segment)
-        points.extend(new_points)
-        startingPoint = points[-1]
+        new_points = get_points_for_segment(startingPoint, segment, steps)
+        points.update(new_points)
+        startingPoint = list(points)[-1]
+        steps = points[startingPoint]
 
     return points
 
@@ -29,12 +36,20 @@ def part1(wire1, wire2):
     wire1_points = get_points(wire1.split(","))
     wire2_points = get_points(wire2.split(","))
 
-    intersections = list(set(wire1_points).intersection(wire2_points))
+    intersections = list(set(wire1_points.keys()).intersection(wire2_points.keys()))
     intersections.remove((0,0))
     return min([abs(point[0]) + abs(point[1]) for point in intersections])
-    
+
+def part2(wire1, wire2):
+    wire1_points = get_points(wire1.split(","))
+    wire2_points = get_points(wire2.split(","))
+
+    intersections = list(set(wire1_points.keys()).intersection(wire2_points.keys()))
+    intersections.remove((0,0))
+    return min(wire1_points[x] + wire2_points[x] for x in intersections)
 
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as raw:
         data = raw.readlines()
         print("#1: ", part1(data[0], data[1]))
+        print("#2: ", part2(data[0], data[1]))
--- 2019/day3/test.py
+++ 2019/day3/test.py
@@ -1,10 +1,16 @@
 import unittest
-from day3 import part1
+from day3 import part1, part2
 
 cases = [
     ("R8,U5,L5,D3","U7,R6,D4,L4",6),
     ("R75,D30,R83,U83,L12,D49,R71,U7,L72","U62,R66,U55,R34,D71,R55,D58,R83",159),
     ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",135)
+]
+
+step_cases = [
+    ("R8,U5,L5,D3","U7,R6,D4,L4",30),
+    ("R75,D30,R83,U83,L12,D49,R71,U7,L72","U62,R66,U55,R34,D71,R55,D58,R83",610),
+    ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51","U98,R91,D20,R16,D67,R40,U7,R15,U6,R7",410)
 ]
 
 class TestSequence(unittest.TestCase):
@@ -13,6 +19,12 @@ class TestSequence(unittest.TestCase):
             with self.subTest():
                 result = part1(wire1, wire2)
                 self.assertEqual(result, distance)
+
+    def test_wire_steps(self):
+        for wire1, wire2, steps in step_cases:
+            with self.subTest():
+                result = part2(wire1, wire2)
+                self.assertEqual(result, steps)
 
 if __name__ == '__main__':
     unittest.main()
