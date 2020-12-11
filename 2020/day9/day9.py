4763cb88b2c258c99fcce5a46e9025f931cde814
Ryan Foley <rtfoley@gmail.com>
2020-12-10 23:30:36 -0500

    add 2020 day 9 part 2 solution

--- 2020/day9/day9.py
+++ 2020/day9/day9.py
@@ -17,9 +17,21 @@ def part1(program, size):
 
         return number
 
+def part2(program):
+    target = 15690279
+    for index in range(len(program)):
+        sum = 0
+        for test_index in range(index, len(program)):
+            number = program[test_index]
+            sum += program[test_index]
+            if sum == target:
+                return min(program[index:test_index]) + max(program[index:test_index])
+            elif sum >= target:
+                continue
+
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as raw:
         data = raw.readlines()
         data = [int(x.replace("\n", "")) for x in data]
         print("#1: ", part1(data, 25))
-        #print("#2: ", part2(data))
+        print("#2: ", part2(data))
